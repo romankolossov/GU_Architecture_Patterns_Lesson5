fe332d5747b708adfeecf6b866a0a100f6363a1f
romankolossov <rkolossov@gmail.com>
2020-11-17 06:39:13 +0300

    app created

---
+++ Composite/Composite/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Composite
+//
+//  Created by Роман Колосов on 17.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
